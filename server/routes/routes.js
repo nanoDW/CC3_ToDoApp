1b0f7e3568fbfc371fc4bcc99ed389e81334c378
czajka.marcin@protonmail.com <czajka.marcin@protonmail.com>
2019-04-21 19:00:09 +0200

    Add POST validation

--- server/routes/routes.js
+++ server/routes/routes.js
@@ -1,43 +1,52 @@
 const express = require("express");
 const bodyParser = require('body-parser')
 const mongoose = require("mongoose");
+const __list = require('../models/list');
+const __task = require('../models/task');
 
 module.exports = function (app) {
-
-    const __list = require('../models/list');
     const List = __list.List;
     const validateList = __list.validate;
-
-    const __task = require('../models/task');
+    
     const Task = __task.Task;
     const validateTask = __task.validate;
 
     const jsonParser = bodyParser.json();
 
-    app.post("/lists", jsonParser, async (req, res) => {
+    app.post("/api/lists", jsonParser, async (req, res) => {
 
         const currentDate = Date.now();
 
         const list = new List({
             name: req.body.name,
             createdAt: currentDate,
-            color: req.body.color,
+            color: req.body.color
         });
+
+        if (validateList(list).error){
+            res.status(400).send(validateList(list).error);
+            return;
+        }
+
         await list.save();
         res.send(list);
     });
 
-    app.get("/lists", async (req, res) => {
-        const lists = await List.find({
-            name: 'list'
-        });
+    app.get("/api/lists", async (req, res) => {
+        const lists = await List.find(c => true);
 
-        if(lists) {
             res.send(lists);
-        }
+    });
+    app.get("/api/lists:_id", async (req, res) => {
+        const lists = await List.find(c => {
+            console.log(c)
+            console.log(req.params)
+            return c._id === req.params._id;
+        });
+            res.send(lists);
     });
 
-    app.post("/tasks", jsonParser, async (req, res) => {
+    app.post("/api/tasks", jsonParser, async (req, res) => {
         
         const task = new Task({
             name: req.body.name,
@@ -46,18 +55,19 @@ module.exports = function (app) {
             deadline: req.body.deadline
         });
 
+        if (validateTask(task).error) {
+            res.status(400).send(validateTask(task).error);
+            return;
+        }
+
         await task.save();
         res.send(task);
     });
 
-    app.get("/tasks", async (req, res) => {
-        const tasks = await Task.find({
-            list: 'ds'
-        });
+    app.get("/api/tasks", async (req, res) => {
+        const tasks = await Task.find(c => true);
 
-        if(tasks) {
             res.send(tasks);
-        }
     });
    
 }
