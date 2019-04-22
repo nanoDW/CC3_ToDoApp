d5bbc4f7acc13003ad2b12bdebca0ff832d5669a
Aaspyr <aaspyr@gmail.com>
2019-04-22 14:58:30 +0200

    CORS

--- server/server.js
+++ server/server.js
@@ -3,15 +3,30 @@ const express = require('express');
 const db = require('./db')();
 const app = express();
 const routes = require('./routes/routes')(app);
+const cors = require('cors');
 const {List, validate} = require('./models/list.js');
 
 
 //server PORT
 const port = process.env.PORT || 3000;
-const server = app.listen(port, () => console.log(`Listening on port ${port}...`))
+const server = app.listen(port, () => {
+    console.log(`CORS-enebled web server is now running...`)
+    console.log(`Listening on port ${port}...`)
+})
 
-//
+//CORS config
+app.use(cors());
+app.use(function (req, res, next) {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    next();
+}); 
+
+//Body request
 app.use(express.json());
+app.use(express.urlencoded({
+    extended: true
+}));
+
 
 //ERROR
 app.get("*", function (req, res) {
