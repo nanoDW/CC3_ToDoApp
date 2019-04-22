25429e0364ae47603e6039c67cb4fba6428030c8
Maciej Korolik <maciej.korolik@gmail.com>
2019-04-22 13:52:38 +0200

    Webpack configuration

---
+++ webpack.config.js
@@ -0,0 +1,41 @@
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+
+module.exports = {
+  entry: "./client/src/index.js",
+  output: {
+    path: path.resolve(__dirname, "dist"),
+    filename: "bundle.js"
+  },
+  module: {
+    rules: [
+      {
+        test: /\.m?js$/,
+        exclude: /(node_modules|bower_components|server)/,
+        use: {
+          loader: "babel-loader",
+          options: {
+            presets: ["@babel/preset-env"]
+          }
+        }
+      },
+      {
+        test: /\.css$/,
+        use: ["style-loader", "css-loader"]
+      },
+      {
+        test: /\.(png|svg|jpg|gif)$/,
+        use: ["file-loader?name=[name].[ext]&outputPath=img/"]
+      },
+      {
+        test: /\.(eot|ttf|woff|woff2)$/,
+        use: ["file-loader?name=[name].[ext]&outputPath=fonts/"]
+      }
+    ]
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: "./client/src/index.html"
+    })
+  ]
+};
