3f6cb71fa8caa63d4aa5ec94a7c92e6b20f48d65
Maciej Korolik <maciej.korolik@gmail.com>
2019-04-25 22:04:31 +0200

    Draft of css styles started, mobile only

--- client/src/index.js
+++ client/src/index.js
@@ -1,1 +1,9 @@
-console.log("webpack test");
+import "./style/style.css";
+
+document.querySelector(".btn--login").addEventListener("click", () => {
+  event.preventDefault();
+  document.querySelector(".login-screen").classList.add("login-screen--hidden");
+  document
+    .querySelector(".main-screen")
+    .classList.remove("main-screen--hidden");
+});
